6060c5d1d7c557fab68106d31f46aceb20b16f12
Dennis Skinner <skinner927@gmail.com>
2018-08-31 15:37:41 -0400

    Quick update before flight. Doesn't do very good error info.

--- index.js
+++ index.js
@@ -15,48 +15,48 @@ const firstName = argv.f;
 const lastName = argv.l;
 
 
-const nightmare = Nightmare({ show: show });
+const nightmare = Nightmare({
+  openDevTools: {
+    mode: 'detach'
+  },
+  show: show
+});
 
 nightmare
-  .goto('https://www.southwest.com/flight/retrieveCheckinDoc.html')
+  .goto('https://www.southwest.com/air/check-in/index.html')
   .wait('input#confirmationNumber')
   .wait(300)
   .type('input#confirmationNumber', confirmationNumber)
-  .type('input#firstName', firstName)
-  .type('input#lastName', lastName)
-  .click('input#submitButton')
-  .wait(function(done) {
-    var checkinButtonEl = 'input#printDocumentsButton',
-      errorsEl = 'ul#errors',
-      tooEarlyTxt = 'The request to check in and print your Boarding Pass is more than 24 hours prior to your scheduled',
-      invalidConf = 'The confirmation number entered is invalid';
+  .type('input#passengerFirstName', firstName)
+  .type('input#passengerLastName', lastName)
+  .click('button#form-mixin--submit-button')
+  .wait('button.air-check-in-review-results--check-in-button')
+  .click('button.air-check-in-review-results--check-in-button')
+  // .wait(function(done) {
+  //   console.log('Waiting for checkin button.');
+  //   var checkinButtonEl = 'button.air-check-in-review-results--check-in-button',
+  //     errorsEl = 'div.page-error h2'; // TODO: this errors is prob wrong
 
-    var err = document.querySelector(errorsEl);
-    if (err) {
-      var txt = err.innerText;
-      if (txt.indexOf(tooEarlyTxt) !== -1) {
-        done('Too Early', null);
-        return;
-      } else if (txt.indexOf(invalidConf) !== -1) {
-        done('Invalid confirmation number', null);
-        return;
+  //   var err = document.querySelector(errorsEl);
+  //   if (err) {
+  //     done(err.innerText || 'UNKNOWN ERROR');
+  //     return;
+  //   }
+
+  //   done(null, !!document.querySelector(checkinButtonEl));
+  // })
+  .wait('.air-check-in-passenger-item--information-boarding-position')
+  .evaluate(function () {
+    var pos = document.querySelectorAll('.air-check-in-passenger-item--information-boarding-position > span');
+
+    var out = [];
+    for (var i = 0; i < pos.length; i++) {
+      var txt = pos[i].innerText.trim();
+      if (txt) {
+        out.push(txt)
       }
     }
-    //console.log('err', err && err.innerText);
-    //console.log('el', !!document.querySelector(checkinButtonEl));
-
-    done(null, !!document.querySelector(checkinButtonEl));
-  })
-  .click('input#printDocumentsButton')
-  .wait('.itinerary_content')
-  .evaluate(function () {
-    return '' + document.querySelector('.boarding_group > .boardingInfo').innerText + 
-      ' ' + document.querySelector('.boarding_position > .boardingInfo').innerText;
-
-    /*
-    <td class="boarding_group"><span class="boardingInfo">A</span></td>
-    <td class="boarding_position"><span class="boardingInfo">46</span></td>
-    */
+    return out.join(', ');
   })
   .end()
   .then(function (result) {
